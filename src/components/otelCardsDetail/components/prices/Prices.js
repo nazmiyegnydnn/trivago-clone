fe110a586df92d5900900f8af48388abc9101b48
nazmiyegnydnn <nazmiyeyilmaz159@gmail.com>
2024-02-29 17:50:43 +0300

    hotel card detail pages design started

---
+++ src/components/otelCardsDetail/components/prices/Prices.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Prices = () => {
+  return (
+    <div className='prices'>
+      FİYAT
+    </div>
+  )
+}
+
+export default Prices
--- src/components/otelCardsTabs/OtelCardsTabs.js
+++ src/components/otelCardsTabs/OtelCardsTabs.js
@@ -1,31 +1,48 @@
-import React from 'react'
-import './OtelCardsTabs.scss'
-import { Tabs } from 'antd';
-import OtelCardsDetail from '../otelCardsDetail/OtelCardsDetail'
+import React from "react";
+import "./OtelCardsTabs.scss";
+import { Tabs } from "antd";
+import Information from "../otelCardsDetail/components/information/Information";
+import Comments from "../otelCardsDetail/components/comments/Comments";
+import Photograph from "../otelCardsDetail/components/photograph/Photograph";
+import Prices from "../otelCardsDetail/components/prices/Prices";
 
 const OtelCardsTabs = () => {
-  const { TabPane } = Tabs;
-  const tabsData = new Array(3).fill(null).map((_, i) => {
-    const id = String(i + 1);
-    return {
-      label: `Tab ${id}`,
-      key: id,
-      name: `Name ${id}`
-    };
-  });
-
+  const onChange = (key) => {
+    console.log(key);
+  };
+  const items = [
+    {
+      key: "1",
+      label: "Bilgi",
+      children: <Information />,
+    },
+    {
+      key: "2",
+      label: "Fotoğraflar",
+      children: <Photograph />,
+    },
+    {
+      key: "3",
+      label: "Yorumlar",
+      children: <Comments />,
+    },
+    {
+      key: "4",
+      label: "Fiyatlar",
+      children: <Prices />,
+    },
+  ];
 
   return (
-    <div className='otelCardsTabs'>
-<Tabs defaultActiveKey="1" centered>
-        {tabsData.map(tab => (
-          <TabPane tab={tab.label} key={tab.key}>
-            <OtelCardsDetail tabKey={tab.key} name={tab.name} />
-          </TabPane>
-        ))}
-      </Tabs>
-   </div>
-  )
-}
+    <div className="otelCardsTabs">
+      <Tabs
+        defaultActiveKey="1"
+        centered
+        items={items}
+        onChange={onChange}
+      ></Tabs>
+    </div>
+  );
+};
 
-export default OtelCardsTabs
+export default OtelCardsTabs;
